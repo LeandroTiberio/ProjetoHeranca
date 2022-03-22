2ab57187c237bb42f509618b1c604383ab962b61
Leandro Tiberio <leandrostiberio@gmail.com>
2022-03-22 20:20:06 -0300

    Adicionando base do projeto

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using ProjetoHeranca.Lanchonete;
+namespace ProjetoHeranca
+{
+    public class Program
+
+    {
+        static void Main(string[] args){
+            
+            Prato[] pratos = new Prato[4];
+            pratos[0] = new Pizza ("Napolitana", true);
+            pratos[1] = new Lanche ("X Bacon", false);
+            pratos[2] = new Salgadinho ("Risoli Carne", false);
+            pratos[3] = new Salgadinho ("Coxinha", true);
+            Pedido pedido = new Pedido("Leandro Tiberio", pratos);
+            Console.WriteLine("O valor total é " + pedido.CalcularValor());
+            Console.WriteLine("Seu troco é " + pedido.RealizarPagamento(100));
+
+        }
+ 
+    }
+}
+
+ 
