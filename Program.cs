779ba221fc7eb3cbe94e395e01b85dab834cd5dd
Leandro Tiberio <leandrostiberio@gmail.com>
2022-03-23 23:03:46 -0300

    Aprendendo novas funcionalidades da classe List

--- Program.cs
+++ Program.cs
@@ -41,8 +41,22 @@ namespace ProjetoHeranca
             var tentativaDois = numeros.RemoveAt(3); // Remove do indice escolhido
             numeros.RemoveAll(numeroARemover => numeroARemover); // Remove todos os itens que atenda
             numeros.RemoveRange(0, 5); // Remove itens em sequencia, parametros a primeira posição e 
+            var indexString = string.IndexOf(Console.ReadLine()); 
+            var indexCinco = numeros.IndexOf(5); // Busca a posição na lista
+            var valorNaPosicaoTres = numeros.ElementAt(3); // Buscar o elemento na posição informada
+            var valorPar = numeros.Find(numero => numero % 2 == 0);
+            var valoresPares = numero.Where(numero => numero % 2 == 0);
+            var valoresParesDois = numero.FindAll(numero => % 2 == 0);
+            var temQuinze = numeros.Any(numero => numero == 15); // Me confima se existe qualquer item que atende minha condição
+            var temQuatorze = numeros.Exists(x => x == 14); // idem ao de cima
+            var todosSaoPares = numeros.All(numero => numero % 2 == 0); // Verifica se TODOS o itens
+            var textoComMaisDeCincoLetras = string.All(numero => string.Count > 5);
+            var ordenadoDescendente = numeros.OrderByDescending(x => x < 5);
+            var ordenadoDescendente = numeros.OrderBy(x => x);
+
+
             Console.WriteLine("Finalizou");
-            
+
         }
     }
 
