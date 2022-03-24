8c09b83357d73080afc698a69f202fd90e98730d
Leandro Tiberio <leandrostiberio@gmail.com>
2022-03-23 22:09:36 -0300

    trabalhando com List e Remove

--- Program.cs
+++ Program.cs
@@ -20,4 +20,32 @@ namespace ProjetoHeranca
     }
 }
 
+using ProjetoHeranca.Lanchonete;
+namespace ProjetoHeranca
+{
+    public class Program
+    {
+
+        static void Main(string[] args){
+
+            var numeros = new List<int>();
+            var numerosMaisDez = new List<int>();
+            var strings = new List<string>();
+            for(var contador = 0; contador < 10; contador++){
+                numeros.Add(contador);
+                numerosMaisDez.Add(contador+10);
+                strings.Add(contador.ToString());
+            }
+            numeros.AddRange(numerosMaisUm);
+            var tentativaUm = numeros.Remove(5); // Remove o valor independente da posição e devolve se removido
+            var tentativaDois = numeros.RemoveAt(3); // Remove do indice escolhido
+            numeros.RemoveAll(numeroARemover => numeroARemover); // Remove todos os itens que atenda
+            numeros.RemoveRange(0, 5); // Remove itens em sequencia, parametros a primeira posição e 
+            Console.WriteLine("Finalizou");
+            
+        }
+    }
+
+
+}
  
